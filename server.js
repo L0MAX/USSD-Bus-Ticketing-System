bf41514e0fafafae2db23c867614dca09ef32e67
ankah donatu <ankahdonatus@gmail.com>
2019-04-26 10:47:30 +0000

    level 3 all destinations

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ app.post('*', (req, res) => {
     2. Sprinter
     3. Toyota Hiace`
     res.send(response)
+
   }else if (text == '1*1') {
     //Level 2 for Accra
     let response = `CON Choose Your VIP Bus Package
@@ -52,47 +53,48 @@ app.post('*', (req, res) => {
         2  Direct  (Ho-Accra)          GHC 23`
         res.send(response)
   }else if (text == '1*3') {
-            let response = `CON Choose Your Toyota Hiace Package
-            1. Transit (Ho-Sogakofe-Accra) GHC 26
-            2  Direct  (Ho-Accra)          GHC 22`
-            res.send(response)
- }else if (text == '2*1') {
-   //Level 2 for Kumasi
+        let response = `CON Choose Your Toyota Hiace Package
+        1. Transit (Ho-Sogakofe-Accra) GHC 26
+        2  Direct  (Ho-Accra)          GHC 22`
+        res.send(response)
+
+              }else if (text == '2*1') {
+              //Level 2 for Kumasi
                 let response = `CON Choose Your VIP Bus Package
                 1. Transit (Ho-Accra-Kumasi) GHC 70
                 2  Direct  (Ho-Accra)          GHC 58`
                 res.send(response)
               }else if (text == '2*2') {
-                    let response = `CON Choose Your Sprinter Package
-                    1. Transit (Ho-Accra-Kumasi) GHC 60
-                    2  Direct  (Ho-Accra)          GHC 50`
-                    res.send(response)
+                let response = `CON Choose Your Sprinter Package
+                1. Transit (Ho-Accra-Kumasi) GHC 60
+                2  Direct  (Ho-Accra)          GHC 50`
+                res.send(response)
               }else if (text == '2*3') {
-                        let response = `CON Choose Your Toyota Hiace Package
-                        1. Transit (Ho-Accra-Kumasi) GHC 63
-                        2  Direct  (Ho-Accra)          GHC 51`
-                        res.send(response)}else if (text == '3*1') {
+                let response = `CON Choose Your Toyota Hiace Package
+                1. Transit (Ho-Accra-Kumasi) GHC 63
+                2  Direct  (Ho-Accra)          GHC 51`
+                res.send(response)}else if (text == '3*1') {
    //Level 2 for Tamale      
-                            let response = `CON Choose Your VIP Bus Package
-                            1. Direct (Ho-Tamale) GHC 120`
-                            res.send(response)
-                          }else if (text == '3*2') {
-                                let response = `CON Choose Your Sprinter Package
-                                1. Transit (Ho-Tamale) GHC 130`
-                                res.send(response)
-                          }else if (text == '3*3') {
-                                    let response = `CON Choose Your Toyota Hiace Package
-                                    1. Transit (Ho-Tamale) GHC 140`
-                                    res.send(response)
-  }else if (text == '1*1*1') {
-    //Level 3 for Accra VIP Bus
-        let response = `CON You have chosen VIP Bus Transit GHC 30. Enter Mobile Money Wallet Number`
-        res.send(response)
-      }else if (text == '1*1*2') {
-        let response = `CON You have chosen VIP Bus Direct GHC 24. Enter Mobile Money Wallet Number`
-        res.send(response)
-      }else if (text == '1*2*1') {
-          //Level 3 for Accra Sprinter
+    let response = `CON Choose Your VIP Bus Package
+    1. Direct (Ho-Tamale) GHC 120`
+    res.send(response)
+  }else if (text == '3*2') {
+    let response = `CON Choose Your Sprinter Package
+    1. Direct (Ho-Tamale) GHC 130`
+    res.send(response)
+  }else if (text == '3*3') {
+    let response = `CON Choose Your Toyota Hiace Package
+    1. Transit (Ho-Tamale) GHC 140`
+    res.send(response)
+          }else if (text == '1*1*1') {
+            //Level 3 for Accra VIP Bus
+            let response = `CON You have chosen VIP Bus Transit GHC 30. Enter Mobile Money Wallet Number`
+            res.send(response)
+          }else if (text == '1*1*2') {
+            let response = `CON You have chosen VIP Bus Direct GHC 24. Enter Mobile Money Wallet Number`
+            res.send(response)
+          }else if (text == '1*2*1') {
+              //Level 3 for Accra Sprinter
               let response = `CON You have chosen Sprinter Transit GHC 28. Enter Mobile Money Wallet Number`
               res.send(response)
             }else if (text == '1*2*2') {
@@ -105,17 +107,41 @@ app.post('*', (req, res) => {
             }else if (text == '1*3*2') {
               let response = `CON You have chosen Toyota Hiace Direct GHC 22. Enter Mobile Money Wallet Number`
               res.send(response)
-      }else if (text == '2*1*1') {
-          // Business logic for first level response
-          let response = `CON Enter Mobile Money Wallet Number`
-          // let response = `END Your account number is ${accountNumber}`
-          res.send(response)
-        }else if (text == '2*1*2') {
-          // Business logic for first level response
-          let response = `CON Enter Mobile Money Wallet Number`
-          // let response = `END Your account number is ${accountNumber}`
-          res.send(response)
-  } else if (text == '1*2') {
+              }else if (text == '2*1*1') {
+              //level 3 for Kumasi VIP bus
+                    let response = `CON You have chosen VIP Bus Transit GHC 70. Enter Mobile Money Wallet Number`
+                    res.send(response)
+              }else if (text == '2*1*2') {
+                    let response = `CON You have chosen VIP Bus Direct GHC 58. Enter Mobile Money Wallet Number`
+                    res.send(response)
+              }else if (text == '2*1*1') {
+              //level 3 for Kumasi Sprinter
+                    let response = `CON You have chosen Sprinter Transit GHC 60. Enter Mobile Money Wallet Number`
+                    res.send(response)
+              }else if (text == '2*1*2') {
+                    let response = `CON You have chosen Sprinter Direct GHC 50. Enter Mobile Money Wallet Number`
+                    res.send(response)
+              }else if (text == '2*1*1') {
+              //level 3 for Kumasi Toyota Hiace
+                    let response = `CON You have chosen Toyota Hiace Transit GHC 63. Enter Mobile Money Wallet Number`
+                    res.send(response)
+              }else if (text == '2*1*2') {
+                    let response = `CON You have chosen Toyota Hiace Direct GHC 51. Enter Mobile Money Wallet Number`
+                    res.send(response)
+
+                  }else if (text == '3*1*1') {
+                    //level 3 for Tamale VIP bus
+                          let response = `CON You have chosen VIP Bus Direct GHC 120. Enter Mobile Money Wallet Number`
+                          res.send(response)
+                    }else if (text == '3*2*1') {
+                    //level 3 for Tamale Sprinter
+                          let response = `CON You have chosen Sprinter Direct GHC 130. Enter Mobile Money Wallet Number`
+                          res.send(response)
+                    }else if (text == '3*3*1') {
+                    //level 3 for Tamale Toyota Hiace
+                          let response = `CON You have chosen Toyota Hiace Direct GHC 140. Enter Mobile Money Wallet Number`
+                          res.send(response)
+                    } else if (text == '1*2') {
     // This is a second level response where the user selected 1 in the first instance
     let balance = 'NGN 10,000'
     // This is a terminal request. Note how we start the response with END
